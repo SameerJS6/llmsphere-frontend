a5430aedb7447614b57b12e84f7fa9d19b84311e
SameerJS6 <singh6.sameer@gmail.com>
2024-05-04 19:21:56 +0530

    add: Dashboard Table Columns

---
+++ app/(main)/dashboard/columns.tsx
@@ -0,0 +1,161 @@
+'use client';
+
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from '@/components/ui/dropdown-menu';
+import { Button } from '@/components/ui/button';
+
+import { ArrowDownUp } from 'lucide-react';
+import { convertUTC } from '@/lib/convertUTC';
+import { DotsVerticalIcon, Pencil1Icon, PlayIcon } from '@radix-ui/react-icons';
+
+import { ColumnDef } from '@tanstack/react-table';
+
+export type Dashboard = {
+  id: string;
+  name: string;
+  prompt_summary: string;
+  validated_on: string;
+  models: string[];
+  eval_type: string;
+  created_at: string;
+};
+
+export const columns: ColumnDef<Dashboard>[] = [
+  //   {
+  //     id: 'select',
+  //     header: ({ table }) => (
+  //       <input
+  //         type="checkbox"
+  //         checked={table.getIsAllPageRowsSelected()}
+  //         className={
+  //           table.getIsSomePageRowsSelected() ? 'indeterminate:bg-red-400' : ''
+  //         }
+  //         onChange={(e) => table.toggleAllPageRowsSelected(e.target.checked)}
+  //         aria-label="Select All"
+  //       />
+  //     ),
+  //     cell: ({ row }) => (
+  //       <input
+  //         type="checkbox"
+  //         checked={row.getIsSelected()}
+  //         onChange={(e) => row.toggleSelected(e.target.checked)}
+  //         aria-label="Select Row"
+  //       />
+  //     ),
+  //   },
+  {
+    accessorKey: 'id',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Id <ArrowDownUp className="ml-2 size-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const value: string = row.getValue('id');
+      return <div className="line-clamp-1 text-xs">{value}</div>;
+    },
+  },
+  {
+    accessorKey: 'name',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Name <ArrowDownUp className="ml-2 size-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: 'prompt_summary',
+    header: 'Prompt Summary',
+    cell: ({ row }) => {
+      const value: string = row.getValue('prompt_summary');
+      return <div className="line-clamp-1">{value}</div>;
+    },
+  },
+  {
+    accessorKey: 'created_at',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Created At <ArrowDownUp className="ml-2 size-4" />
+        </Button>
+      );
+    },
+
+    size: 300,
+    cell: ({ row }) => {
+      const value: string = row.getValue('created_at');
+      const formattedTime = convertUTC(value);
+      return (
+        <div className="w-24" suppressHydrationWarning>
+          {formattedTime}
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: 'validated_on',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Validated on
+          <ArrowDownUp className="ml-2 size-4" />
+        </Button>
+      );
+    },
+    size: 200,
+  },
+  {
+    accessorKey: 'models',
+    header: 'Models',
+  },
+  {
+    accessorKey: 'eval_type',
+    header: 'Eval type',
+  },
+  {
+    accessorKey: 'run',
+    header: 'Run',
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" size="icon">
+              <DotsVerticalIcon fontSize={16} />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem>
+              <PlayIcon fontSize={16} className="mr-2" /> Run
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Pencil1Icon fontSize={16} className="mr-2" /> Edit
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
