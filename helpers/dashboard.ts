b8292464f3f0a828a155c165cacef1df620bef6f
SameerJS6 <singh6.sameer@gmail.com>
2024-05-06 11:07:27 +0530

    add: dashboard api helper

---
+++ helpers/dashboard.ts
@@ -0,0 +1,17 @@
+import { TDashboard } from '@/types/dashboard.types';
+
+export const getDashboardPrompt = async (): Promise<TDashboard[] | null> => {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/getpromptforuser?username=nitindhir`,
+      {
+        method: 'GET',
+      }
+    );
+    const responseData = await res.json();
+    return responseData;
+  } catch (error) {
+    console.log('Error while Getting Prompt for Dashboard: ', error);
+    throw error;
+  }
+};
