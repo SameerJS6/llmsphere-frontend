b0e6126451e33b8f944c64e8ffa384004cc55616
SameerJS6 <singh6.sameer@gmail.com>
2024-05-10 10:46:40 +0530

    adds: eval test data & task type util

---
+++ lib/evalData.ts
@@ -0,0 +1,50 @@
+export const apiResponse = [
+  {
+    id: '06546230-f96e-4792-bfae-af6048eb5918',
+    evaluation: {
+      task_id: '2',
+      prompt_id: '2505e35f-b62f-42b4-8e8b-38a76004fb57',
+      openai_faithful_score: 1.0,
+      gemini_similarity: 0.8499848656660862,
+      gemini_answer:
+        '**Consumer Name:** Kuldeep**Total Amount Pending:** Rs. 1492',
+      openai_answer:
+        "1. Consumer's name: Kuldeep\n2. Total pending amount: Rs. 1492",
+      executed_on: '2024-05-05 16:20:17',
+      gemini_faithful_score: 1.0,
+      openai_similarity: 0.8625554847587696,
+      username: 'nitindhir',
+    },
+  },
+  {
+    id: '87ecf856-c0d0-41fe-9dd1-130c5c228eb7',
+    evaluation: {
+      prompt_id: '5ee2dede-315a-4e3f-ad79-0066fabc04bd',
+      openai_faithful_score: 1.0,
+      gemini_similarity: 0.852656518723594,
+      gemini_answer:
+        "Plane seats are becoming smaller, with some airlines offering as little as 28 inches of legroom. This could pose safety risks, as it may affect passengers' ability to evacuate quickly in an emergency. Currently, the government sets standards for animal space on planes but not for humans. Experts argue that it's time for the DOT and FAA to take a stand on humane treatment of passengers.",
+      openai_answer:
+        '"Experts raise concerns about shrinking space on planes, posing risks to passenger health and safety. FAA tests show some airlines offering as little as 28 inches pitch. Calls for standards to protect passengers amidst cramped conditions. Consumer group criticizes lack of regulations compared to animal rights on flights. Airlines like United, Air Asia, and Spirit offer limited seat room. Questioning if crowding could lead to serious safety issues beyond discomfort."',
+      executed_on: '2024-05-05 15:02:02',
+      gemini_faithful_score: 1.0,
+      openai_similarity: 0.8796566924603867,
+      username: 'nitindhir',
+    },
+  },
+  {
+    id: 'fb4fbb23-4714-4de7-9d67-3a95f2bbd091',
+    evaluation: {
+      prompt_id: '2505e35f-b62f-42b4-8e8b-38a76004fb57',
+      openai_faithful_score: 1.0,
+      gemini_similarity: 0.8480064795684783,
+      gemini_answer: 'Consumer Name: Kuldeep\nTotal Amount Pending: Rs. 1492',
+      openai_answer:
+        "1. Consumer's name: Kuldeep\n2. Total pending amount: Rs. 1492",
+      executed_on: '2024-05-05 15:03:21',
+      gemini_faithful_score: NaN,
+      openai_similarity: 0.8625554847587696,
+      username: 'nitindhir',
+    },
+  },
+];
