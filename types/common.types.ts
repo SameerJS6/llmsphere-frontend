65b90809a4246918ea2a5e565ceb68da6508a65a
Tushar-k-minny <tushark0134@gmail.com>
2024-05-11 17:06:36 +0530

    add: BarChart component

--- types/common.types.ts
+++ types/common.types.ts
@@ -1,4 +1,19 @@
 export interface IFrameworkModels {
-    value: string;
-    label: string;
-  }
+  value: string;
+  label: string;
+}
+
+export interface YData {
+  label: string;
+  data: number[];
+  color: string;
+}
+/*sample data for ydata
+
+ {
+      label: 'Similarity Score',
+      color: 'rgba(57, 255, 20, 1)',
+      data: [20, 50, 25], //first for OpenAi, second for gemini
+    },
+
+*/
