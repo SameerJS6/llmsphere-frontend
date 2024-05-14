eaf49b032a98b3ab259ae46446f17c0a7ecc0d5c
SameerJS6 <singh6.sameer@gmail.com>
2024-05-14 15:48:27 +0530

    add: report score card comp

---
+++ app/(main)/eval-dashboard/[id]/report/_component/score-card.tsx
@@ -0,0 +1,24 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import React from 'react';
+
+type ScoreCardProps = {
+  title: string;
+  score: string;
+};
+
+export default function ScoreCard({ title, score }: ScoreCardProps) {
+  return (
+    <Card className="flex-1">
+      <CardHeader>
+        <CardTitle className="line-clamp-1 bg-gradient-to-b from-foreground to-muted-foreground bg-clip-text text-transparent">
+          {title}
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <h2 className="text-2xl font-semibold tracking-tight">
+          {Number(score).toFixed(4)}
+        </h2>
+      </CardContent>
+    </Card>
+  );
+}
