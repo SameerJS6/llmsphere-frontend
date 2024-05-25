c437b670484b8882c8dcda44abe95437c66b2931
SameerJS6 <singh6.sameer@gmail.com>
2024-05-25 12:38:13 +0530

    fix: eval prompt arena support for problem statement

--- app/(main)/eval-prompts/[id]/page.tsx
+++ app/(main)/eval-prompts/[id]/page.tsx
@@ -36,54 +36,62 @@ export default async function EvalPrompt({
 }: EvalPromptProps) {
   const PromptData = await getPromptById(params.id);
   let isEdit: boolean = false;
+  const promptType = !!PromptData?.problem ? 'Problem statement' : 'Prompt';
+
+  const isOpenAIUsed = !!PromptData?.OpenAI || !!PromptData?.openai_prompt;
+  const isGeminiUsed = !!PromptData?.gemini || !!PromptData?.gemini_prompt;
 
   searchParams?.mode === 'edit' ? (isEdit = true) : (isEdit = false);
-  // const [loading,setLoading] = useState<boolean>(false);
-
-  //   console.log(PromptData, 'from prompt');
-
-  // const { activePromptMode } = usePromptArenaContext();
-
   return (
     <div className="relative z-10 mt-8 space-y-8">
       <div className="flex items-center justify-between gap-4">
         <h2 className="bg-gradient-to-b from-foreground to-muted-foreground bg-clip-text text-2xl font-semibold tracking-tight text-transparent">
-          Prompt Arena
+          Evaluate Prompt
         </h2>
         {!isEdit && <EvalPromptTaskDropdown />}
-
-        {/* <PromptModeToggle /> */}
       </div>
       <div className="grid h-full items-stretch gap-6 md:grid-cols-[1fr_300px]">
         <div className="space-y-4 rounded-lg border border-border bg-accent p-4 text-accent-foreground">
-          <div className="grid gap-4 lg:grid-cols-2">
+          {PromptData?.problem && (
             <div className="relative h-full space-y-2">
-              <Label htmlFor="key">OpenAI</Label>
+              <Label htmlFor="key">Problem Statement</Label>
               <EvalPromptInput
                 isEdit={isEdit}
-                promptText={PromptData?.openai_prompt}
+                promptText={PromptData?.problem}
                 model="OpenAI"
               />
             </div>
-            <div className="relative h-full space-y-2">
-              <Label htmlFor="key">Gemini</Label>
-              <EvalPromptInput
-                isEdit={isEdit}
-                promptText={PromptData?.gemini_prompt}
-                model="Gemini"
-              />
+          )}
+          {!PromptData?.problem && (
+            <div className="grid gap-4 lg:grid-cols-2">
+              <div className="relative h-full space-y-2">
+                <Label htmlFor="key">OpenAI</Label>
+                <EvalPromptInput
+                  isEdit={isEdit}
+                  promptText={PromptData?.openai_prompt}
+                  model="OpenAI"
+                />
+              </div>
+              <div className="relative h-full space-y-2">
+                <Label htmlFor="key">Gemini</Label>
+                <EvalPromptInput
+                  isEdit={isEdit}
+                  promptText={PromptData?.gemini_prompt}
+                  model="Gemini"
+                />
+              </div>
             </div>
-          </div>
+          )}
         </div>
         <EvalArenaRightColumn
           variable_name={PromptData?.variable_name}
           variable_value={PromptData?.variable_value}
-          openai_prompt={PromptData?.openai_prompt}
-          gemini_prompt={PromptData?.gemini_prompt}
+          isOpenAI={isOpenAIUsed}
+          isGemini={isGeminiUsed}
           isEdit={isEdit}
         />
       </div>
-      <EvalArenaFooter isEdit={isEdit} id={params.id} />
+      <EvalArenaFooter isEdit={isEdit} id={params.id} promptType={promptType} />
     </div>
   );
 }
