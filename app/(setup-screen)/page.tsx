b375135793a520d421adef86a4fbc9205e1df37d
SameerJS6 <singh6.sameer@gmail.com>
2024-05-27 10:19:49 +0530

    fix: Integration redirection

--- app/(setup-screen)/page.tsx
+++ app/(setup-screen)/page.tsx
@@ -1,8 +1,11 @@
 import LLMCard from './_components/llmcard';
 import IntegrateButton from './_components/IntegrateButton';
-import { redirect } from 'next/navigation'
+import { redirect } from 'next/navigation';
 import { getCredentials } from '@/helpers/auth';
 import { PlusCircledIcon } from '@radix-ui/react-icons';
+import { Button } from '@/components/ui/button';
+import { ArrowRight, MoveRight } from 'lucide-react';
+import Link from 'next/link';
 
 let openai = '';
 let googleai = '';
@@ -15,18 +18,15 @@ async function fetchData() {
       if (data?.credentials?.Google_AI) googleai = data?.credentials?.Google_AI;
       else if (data?.credentials?.Google_AIStudio)
         googleai = data?.credentials?.Google_AIStudio;
-      
     }
-    } catch (error) {
+  } catch (error) {
     console.error('Error fetching data:', error);
   }
-} 
+}
 
 export default async function SetupScreen() {
   await fetchData();
-  if(openai && googleai){
-    redirect('/prompt')
-  }
+  const isIntegrated: boolean = !!openai && !!googleai;
   return (
     <>
       <main className="relative flex min-h-[calc(100dvh-68px)] w-full flex-col justify-center bg-dot-black/[0.2] dark:bg-dot-white/[0.2]">
@@ -76,7 +76,15 @@ export default async function SetupScreen() {
             />
           </div>
           <div className="text-right">
-            <IntegrateButton />
+            {isIntegrated ? (
+              <Button className="gap-2" asChild>
+                <Link href="/prompt">
+                  Go to Prompt Arena <ArrowRight className="size-4" />
+                </Link>
+              </Button>
+            ) : (
+              <IntegrateButton />
+            )}
           </div>
         </div>
       </main>
